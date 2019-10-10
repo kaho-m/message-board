bc1e7ae9bc937e4bc234276de2f01da87d52aac6
tsukada <f117karasu@gmail.com>
2019-10-10 06:29:17 +0000

    validation

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -13,7 +13,7 @@
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
                 
-                {!! Form::submit('投稿', ['class', => 'btn btn-primary']) !!}
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
                 
             {!! Form::close() !!}
         </div>
